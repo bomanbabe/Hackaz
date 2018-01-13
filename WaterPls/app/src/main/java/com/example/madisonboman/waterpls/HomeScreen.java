7f8c90041db510234e0940a93846d64a34a1ae8d
Madison Boman <madisonboman@MadisMacBookPro.arizona.edu>
2018-01-13 02:04:56 -0700

    jkfbdjdsbf

--- WaterPls/app/src/main/java/com/example/madisonboman/waterpls/HomeScreen.java
+++ WaterPls/app/src/main/java/com/example/madisonboman/waterpls/HomeScreen.java
@@ -1,29 +1,62 @@
 package com.example.madisonboman.waterpls;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
+    static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
+    }
+
+    public void getLocation(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else{
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            if(location != null){
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
+
+                ((TextView)findViewById(R.id.textView)).setText("Latitude: " + lat);
+                ((TextView)findViewById(R.id.textView2)).setText("Longitude: " + lon);
+            } else{
+                ((TextView)findViewById(R.id.textView)).setText("Couldnt get location!");
+                ((TextView)findViewById(R.id.textView2)).setText("Couldnt get location!");
             }
-        });
+
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        switch(requestCode){
+            case REQUEST_LOCATION:
+                getLocation();
+                break;
+        }
     }
 
 }
