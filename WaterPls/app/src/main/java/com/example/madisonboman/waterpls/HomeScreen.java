5761a77bc3eed73235c7e3865b7ec1b4a74691a9
Madison Boman <madisonboman@MadisMacBookPro.arizona.edu>
2018-01-13 16:05:28 -0700

    added http req func

--- WaterPls/app/src/main/java/com/example/madisonboman/waterpls/HomeScreen.java
+++ WaterPls/app/src/main/java/com/example/madisonboman/waterpls/HomeScreen.java
@@ -3,60 +3,17 @@ package com.example.madisonboman.waterpls;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.location.LocationManager;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-public class HomeScreen extends AppCompatActivity {
-    static final int REQUEST_LOCATION = 1;
-    LocationManager locationManager;
+public class HomeScreen extends AppCompatActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        getLocation();
-    }
-
-    public void getLocation(){
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        } else{
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            if(location != null){
-                double lat = location.getLatitude();
-                double lon = location.getLongitude();
-
-                ((TextView)findViewById(R.id.textView)).setText("Latitude: " + lat);
-                ((TextView)findViewById(R.id.textView2)).setText("Longitude: " + lon);
-            } else{
-                ((TextView)findViewById(R.id.textView)).setText("Couldnt get location!");
-                ((TextView)findViewById(R.id.textView2)).setText("Couldnt get location!");
-            }
-
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        switch(requestCode){
-            case REQUEST_LOCATION:
-                getLocation();
-                break;
-        }
-    }
+    double lat = 37.4220;
+    double lon = -122.0840;
 
 }
